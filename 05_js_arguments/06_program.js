110f6c8b47aeab44ebbe7c49819e2034198116a7
KeerthanaA0329 <keerthanaaruchamy03292@gmail.com>
2024-07-14 19:48:24 +0530

    javascript

---
+++ 05_js_arguments/06_program.js
@@ -0,0 +1,31 @@
+// A function with two arguments
+
+var showQuotient;
+
+showQuotient = function (number1, number2) {
+  var result = number1 / number2;
+  console.log("The quotient is " + result);
+};
+
+showQuotient(10, 2);  
+showQuotient(15, 3);   
+showQuotient(8, 2.5);  
+
+/* Further Adventures
+ *
+ * 1) Use the showSum function to add 56 and 74.
+ *
+ * To multiply two numbers, use the * symbol.
+ * e.g. 3 * 5 is 3 multiplied by 5.
+ *
+ * 2) Create a showProduct function to multiply two numbers.
+ *
+ * 3) Use your function to multiply three pairs of numbers.
+ *
+ * To divide one number by another, use /.
+ * e.g. 10 / 2 is 10 divided by 2.
+ *
+ * 4) What about showDifference and showQuotient
+ *    for subtraction and division?
+ *
+ */
