6ecc05ba9b01474acd2cfbf7c96e3ad97c077d8c
KeerthanaA0329 <keerthanaaruchamy03292@gmail.com>
2024-07-14 19:33:32 +0530

    javascript

---
+++ 05_js_arguments/02_program.js
@@ -0,0 +1,31 @@
+// Breaking a function by changing a variable name
+
+var msg;
+var msg2;
+var showMessage;
+
+msg = "It's full of stars!";
+msg2 = "Look at all those stars!";
+
+showMessage = function () {
+	console.log(msg + " " + msg2);
+};
+
+showMessage();
+;
+
+
+
+/* Further Adventures
+ *
+ * 1) Update the console.log so the program works.
+ *
+ * 2) Declare another message variable
+ *    and assign it a value.
+ *
+ * 3) Display the new message as well as the old one.
+ *
+ * 4) Display a single message created by joining
+ *    the two strings.
+ *
+ */
