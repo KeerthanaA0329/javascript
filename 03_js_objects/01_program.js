33b1cbd6ab7b24080af4f92233761ac8da006fa2
KeerthanaA0329 <keerthanaaruchamy0329@gmail.com>
2024-07-14 13:43:02 +0530

    javascript

---
+++ 03_js_objects/01_program.js
@@ -0,0 +1,62 @@
+// Using variables to represent a book
+
+var bookTitle;
+var bookAuthor;
+
+bookTitle = "The Hobbit";
+bookAuthor = "J. R. R. Tolkien";
+
+console.log(bookTitle + " by " + bookAuthor);
+var bookTitle2 = "Harry Potter and the Philosopher's Stone";
+var bookAuthor2 = "J. K. Rowling";
+
+console.log(bookTitle2 + " by " + bookAuthor2);
+var bookTitle3 = "1984";
+var bookAuthor3 = "George Orwell";
+
+console.log(bookTitle3 + " by " + bookAuthor3);
+var bookTitle4 = "To Kill a Mockingbird";
+var bookAuthor4 = "Harper Lee";
+console.log(bookTitle4 + " by " + bookAuthor4);
+
+var bookTitle5 = "The Great Gatsby";
+var bookAuthor5 = "F. Scott Fitzgerald";
+console.log(bookTitle5 + " by " + bookAuthor5);
+
+var bookTitle6 = "Pride and Prejudice";
+var bookAuthor6 = "Jane Austen";
+console.log(bookTitle6 + " by " + bookAuthor6);
+
+var bookTitle7 = "The Catcher in the Rye";
+var bookAuthor7 = "J. D. Salinger";
+console.log(bookTitle7 + " by " + bookAuthor7);
+
+var bookTitle8 = "The Lord of the Rings";
+var bookAuthor8 = "J. R. R. Tolkien";
+console.log(bookTitle8 + " by " + bookAuthor8);
+
+var bookTitle9 = "Moby Dick";
+var bookAuthor9 = "Herman Melville";
+console.log(bookTitle9 + " by " + bookAuthor9);
+
+var bookTitle10 = "The Chronicles of Narnia";
+var bookAuthor10 = "C. S. Lewis";
+console.log(bookTitle10 + " by " + bookAuthor10);
+
+
+
+
+
+/* Further Adventures
+ *
+ * 1) Declare variables for a second book
+ *    and assign them values.
+ *
+ * 2) Add code to log its details to the console.
+ *
+ * 3) Repeat steps 1 and 2 for a third book.
+ *
+ * 4) Consider the code needed for 10 books.
+ *    For 100 books.
+ *
+ */
